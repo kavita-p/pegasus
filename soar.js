962ad9b78d80e7e19a66aeb6c5df7fff71a3ae39
Kavita Poduri <76273926+kavita-p@users.noreply.github.com>
2023-04-21 15:52:32 -0700

    polish the boy up so he's ready to move

--- soar.js
+++ soar.js
@@ -41,9 +41,7 @@ const { JSDOM } = jsdom;
   <sup>${poem.source}</sup>
   
   <hr/>
-  <sup>I'm Pegasus! I fetch the Poetry Foundation's Poem of the Day and crosspost it to cohost. Find more details about me here.</sup>`;
-
-  // console.log(postContent);
+  <sup>I'm Pegasus! I fetch the Poetry Foundation's Poem of the Day and crosspost it to cohost. Find more details about me [here](https://cohost.org/pegasus-poetry/post/1372999-i-m-pegasus).</sup>`;
 
   // posting
   let user = new cohost.User();
@@ -61,6 +59,6 @@ const { JSDOM } = jsdom;
     adultContent: false,
     blocks: [{ type: "markdown", markdown: { content: postContent } }],
     cws: [],
-    tags: [poem.authorTag.substring(3)],
+    tags: [poem.authorTag.substring(3), "poetry", "poetry foundation"],
   });
 })();
